20f545f95e71465137a7b2d201ceea5e2c47717c
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2021-12-22 23:30:05 +0600

    wrote the functions

--- src/views/BookList.js
+++ src/views/BookList.js
@@ -5,16 +5,29 @@ import {Button} from "react-bootstrap"
 
 export default function BookList() {
   const [books, setBooks] = useState(data);
+
+  function upvote(book){
+    let resultList = books.filter(p=>p!==book)
+    book.likes+=1
+    resultList=[...resultList, book]
+    resultList.sort((a, b)=>b.likes-a.likes)
+    setBooks(resultList)
+  }
+
+  useEffect(()=>{
+      let resultList = books
+      resultList.sort((a, b) => b.likes-a.likes)
+      setBooks(resultList)
+  }, [])
+
   return (
     <>
       {console.log(books)}
       {books.map((book) => (
         <>
           <Book book={book} />
-          <Button variant="success">
-            <p>{book.likes}</p>
-          </Button>
-          <hr />
+          <Button variant="success" onClick={()=> upvote(book) }>{book.likes}</Button>
+          <hr/>
         </>
       ))}
     </>
